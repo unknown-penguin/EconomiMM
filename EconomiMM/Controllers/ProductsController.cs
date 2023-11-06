57cb3dd0e8359fcbe4f6d683a63a93d7a18cf907
Valeriy Tsyganok <tsyganok4@gmail.com>
2023-11-06 20:11:57 +0200

    removed useless code

--- EconomiMM/Controllers/ProductsController.cs
+++ EconomiMM/Controllers/ProductsController.cs
@@ -66,57 +66,7 @@ namespace EconomiMM.Controllers
             }
             return View(product);
         }
-
-        // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Orders == null)
-            {
-                return NotFound();
-            }
-
-            var product = await _context.Orders.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int? id, [Bind("Id,orderNumber,temperature,jointShape,jointType,withLiner,mainPartLength1,mainPartDiameter,mainPartLength2,mainPartWidth,flangeWidth,koef,siliconeTubes,linerPartLength,linerPartHeight,linerPartWidth,linerPartBindingWidth,mainPartWorkPrice,linerPartWorkPrice,TotalPrice")] Product product)
-        {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(product.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(product);
-        }
+       
 
         // GET: Products/Delete/5
         public async Task<IActionResult> Delete(int? id)
--- EconomiMM/Models/Product.cs
+++ EconomiMM/Models/Product.cs
@@ -1,4 +1,6 @@
 ﻿using EconomiMM.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace EconomiMM.Models
 {
@@ -6,43 +8,71 @@ namespace EconomiMM.Models
     {
         public int? Id { get; set; }
 
-        public string orderNumber { get; set; }   
+        [Display(Name = "Номер замовлення")]
+        public string orderNumber { get; set; }
 
+        [Display(Name = "Температура")]
         public string temperature { get; set; }
 
+        [Display(Name = "Форма компенсатора")]
         public JointShapes jointShape { get; set; }
 
+        [Display(Name = "Тип компенсатора")]
         public JointType jointType { get; set; }
 
+        [Display(Name = "З вкладишем")]
         public bool withLiner { get; set; }
 
+        [Display(Name = "Довжина головної частини 1")]
         public float? mainPartLength1 { get; set; }
+
+        [Display(Name = "Діаметр головної частини")]
         public float? mainPartDiameter { get; set; }
+
+        [Display(Name = "Довжина головної частини 2")]
         public float mainPartLength2 { get; set; }
+
+        [Display(Name = "Ширина головної частини")]
         public float mainPartWidth { get; set; }
 
+        [Display(Name = "Ширина фланця")]
         public float flangeWidth { get; set; }
+
+        [Display(Name = "Коефіцієнт")]
         public float koef { get; set; }
 
+        [Display(Name = "Силікон")]
         public int siliconeTubes { get; set; }
 
-
+        [Display(Name = "Довжина вкладиша")]
         public float linerPartLength { get; set; }
+
+        [Display(Name = "Висота вкладиша")]
         public float linerPartHeight { get; set; }
+
+        [Display(Name = "Ширина вкладиша")]
         public float linerPartWidth { get; set; }
+
+        [Display(Name = "Ширина кріплення вкладиша")]
         public float linerPartBindingWidth { get; set; }
 
+        [Display(Name = "Вартість роботи головної частини")]
         public float mainPartWorkPrice { get; set; }
+
+        [Display(Name = "Вартість роботи вкладиша")]
         public float linerPartWorkPrice { get; set; }
 
-        public List<SelectedMaterial<ExpansionJointMaterial>>? ExpansionJointsMaterials { get; set; } = new List<SelectedMaterial<ExpansionJointMaterial>>();
-        public List<SelectedMaterial<FlangeMaterial>>? flangeMaterials { get; set; } = new List<SelectedMaterial<FlangeMaterial>>();
         
+        public List<SelectedMaterial<ExpansionJointMaterial>>? ExpansionJointsMaterials { get; set; } = new List<SelectedMaterial<ExpansionJointMaterial>>();
+
+        
+        public List<SelectedMaterial<FlangeMaterial>>? flangeMaterials { get; set; } = new List<SelectedMaterial<FlangeMaterial>>();
+
         public List<SelectedMaterial<LinerExpansionJointMaterial>>? LinerExpansionJointMaterials { get; set; } = new List<SelectedMaterial<LinerExpansionJointMaterial>>();
 
+        [Display(Name = "Загальна вартість")]
         public float TotalPrice { get; set; } = 0;
-        // S
-        
+
 
     }
     
